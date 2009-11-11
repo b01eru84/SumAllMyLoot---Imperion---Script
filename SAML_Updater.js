c0accb3fde43e213ef9e20619c0528c77c9098d8
b01eru84 <b01eru84@yahoo.com>
2009-11-11 18:45:55 +0200

    v1.01

---
+++ SAML_Updater.js
@@ -0,0 +1,33 @@
+/*
+	SumAML - Updater script
+	
+*/
+
+
+function CheckForUpdate(){
+	var ScriptId = 60769;
+	GM_xmlhttpRequest ({
+		method: "GET",
+		url: "http://userscripts.org/scripts/show/" + ScriptId,
+		headers: {
+					"User-agent": "Mozilla/5.0",
+					"Accept": "text/html",
+				 },
+		onload: function (respuesta){
+			var userScripts = document.implementation.createDocument ("", "", null);
+			var html = document.createElement ("html");
+			html.innerHTML = respuesta.responseText;
+			userScripts.appendChild (html);
+			
+			//Get new version
+			var newVersion = userScripts.getElementById ("summary").getElementsByTagName("b")[1].nextSibling.textContent;
+			
+			//Get the name of the script
+			var name = userScripts.getElementById ("details").childNodes[1].innerHTML;
+			
+			if (trim (newVersion) != trim (version)){
+			}
+		}
+	});
+}
+
