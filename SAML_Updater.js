34a23c52556cb3690508d3086b91e5d830b3e029
b01eru84 <b01eru84@yahoo.com>
2009-11-13 15:14:14 +0200

    a

--- SAML_Updater.js
+++ SAML_Updater.js
@@ -5,41 +5,42 @@
 
 //var global = this;
 
-function CheckForUpdate(){
-var version = metadata.setting('version');
-alert(version);
-exit;
-try{
-		
-	var ScriptId = 60769;
-	GM_xmlhttpRequest ({
-		method: "GET",
-		url: "http://userscripts.org/scripts/show/" + ScriptId,
-		headers: {
-					"User-agent": "Mozilla/5.0",
-					"Accept": "text/html",
-				 },
-		onload: function (respuesta){
-			var userScripts = document.implementation.createDocument ("", "", null);
-			var html = document.createElement ("html");
-			html.innerHTML = respuesta.responseText;
-			userScripts.appendChild (html);
-			
-			//Get new version
-			var newVersion = userScripts.getElementById ("summary").getElementsByTagName("b")[1].nextSibling.textContent;
-			
-			//Get the name of the script
-			var name = userScripts.getElementById ("details").childNodes[1].innerHTML;
-			
-			if (trim (newVersion) != trim (version)){
-				alert('avemversiunenoua');
+
+function TestUpdate(){
+	var version = metadata.setting('version');
+	alert(version);
+}
+
+function CheckForUpdate(){	
+	try{		
+		var ScriptId = 60769;
+		GM_xmlhttpRequest ({
+			method: "GET",
+			url: "http://userscripts.org/scripts/show/" + ScriptId,
+			headers: {
+						"User-agent": "Mozilla/5.0",
+						"Accept": "text/html",
+					 },
+			onload: function (respuesta){
+				var userScripts = document.implementation.createDocument ("", "", null);
+				var html = document.createElement ("html");
+				html.innerHTML = respuesta.responseText;
+				userScripts.appendChild (html);
+				
+				//Get new version
+				var newVersion = userScripts.getElementById ("summary").getElementsByTagName("b")[1].nextSibling.textContent;
+				
+				//Get the name of the script
+				var name = userScripts.getElementById ("details").childNodes[1].innerHTML;
+				
+				if (trim (newVersion) != trim (version)){
+					alert('avemversiunenoua');
+				}
 			}
-		}
-	});
-}
-catch(e)
-{
-alert(e);
-}
+		});
+	}
+	catch(e){
+		alert(e);
+	}
 }
 
