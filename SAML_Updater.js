f727431dde30536d04ea64d56813f85826ebb5d2
b01eru84 <b01eru84@yahoo.com>
2009-11-13 15:56:08 +0200

    a

--- SAML_Updater.js
+++ SAML_Updater.js
@@ -5,14 +5,15 @@
 
 
 function addUpdateMessage(){
-	var headMainDiv = document.getElementById('head');
-	alert(headMainDiv);
-	if (headMainDiv){	
-		headMainDiv.innerHTML +=
-			"<div align='center' class='floatLeft ' style='width: 100%; height: 24px;'>"+
-				"<a style='cursor: pointer;' class='colorLightGrey'>Update SAML to new version :1.0001</a>"+
-			"</div>";
-	}
+	alert('1');
+	//var headMainDiv = document.getElementById('head');
+	//alert(headMainDiv);
+	//if (headMainDiv){	
+	//	headMainDiv.innerHTML +=
+	//		"<div align='center' class='floatLeft ' style='width: 100%; height: 24px;'>"+
+	//			"<a style='cursor: pointer;' class='colorLightGrey'>Update SAML to new version :1.0001</a>"+
+	//		"</div>";
+	//}
 }
 
 addUpdateMessage();
