fa0089fe9cff63c777cbed1ce683c80399b794cb
b01eru84 <b01eru84@yahoo.com>
2009-11-19 19:59:11 +0200

    John

--- SAML_MainClass.js
+++ SAML_MainClass.js
@@ -5,17 +5,32 @@
 */
 
 var bannedPlayers = new Array("bgd2006","");
-var goldenPlayers = new Array("b01eru84","");
+var goldenPlayers = new Array("b01eru84","Mishu","chelus","speed2333");
+
+Array.prototype.Contains = function(text){
+	for (var i=0;i<this.length;i++){
+		if (this[i] == text){
+			return true;
+			break;
+		}		
+	}
+	return false;
+}
 
 function getPlayersName(){
 	var element = document.getElementsByClassName('floatLeft marginLeft15 width450')[0].getElementsByTagName ("li")[1].getElementsByTagName ("a")[0].innerHTML.trim();
-	for (var i=0;i<bannedPlayers.length;i++){
-		if (bannedPlayers[i] == element)
+	//for (var i=0;i<bannedPlayers.length;i++){
+	//	if (bannedPlayers[i] == element){
+	if (bannedPlayers.Contains(element)){
 			alert("You cann't use this script!!!");
-	}
-//	if (element)
-		//alert(element);
+			FuckYou();
+			}
+	//	}
+	//}
 	
+		if (goldenPlayers.Contains(element)){
+			//alert("merge");
+		}
 }
 
-getPlayersName();
+//getPlayersName();
