de5cbe949b0a6089aabd20ddc58c310e42e0c505
B01eru84 <b01eru84@yahoo.com>
2009-11-28 22:44:42 +0200

    John

--- SAML_ResearchCenter.js
+++ SAML_ResearchCenter.js
@@ -17,37 +17,31 @@ function showOnResearchCenter(){
 		
 		for(var i = 1; i < researchTables.length; i++)
 		{
-			try
-			{
-				//total points
-				var currPointsDiv = researchTables[i].rows[0].cells[2].getElementsByClassName('researchPoints fontSize18 colorWhite fontBold fontColorRace bgNoRepeat 	interface_fleet_table_background')[0];
-				currPoints[i] = parseFloat(currPointsDiv.innerHTML.match(/\d+/g).join("."));
-				//production / day
-				var points = researchTables[i].rows[2].cells[0].innerHTML.match(/\d+/g).join(".");
-				productionPoints[i] = parseFloat(points);
-				
-				var extraTableDiv = researchCenterMainDiv.getElementsByClassName('extraTable')[i];		
-				var categorieDiv = extraTableDiv.getElementsByClassName('fleetTable bgNoRepeat interface_fleet_table_background ')[0];		
-				var researchTable = categorieDiv.getElementsByClassName('colorWhite')[0];
-				for (var y = 2; y < researchTable.rows.length; y+=2)
-				{	
-					try
-					{
-						var requiredPoints = researchTable.rows[y].cells[2].innerHTML.match(/\d+/g).join("");				
-						var theButton = researchTable.rows[y].cells[3];				
-						var button = theButton.getElementsByClassName('buttonError colorError interface_element_button_error')[0];				
-						var days = showDays(((parseFloat(requiredPoints) - parseFloat(currPoints[i])) / parseFloat(productionPoints[i])).toFixed(2));
-						button.innerHTML = "<span class='interface_element_button_error'></span><div class='ColorGray'>Ready in " + days+"</div>";
-					}
-					catch (e)
-					{
-						window.status = e;
-					}						
+			//total points
+			var currPointsDiv = researchTables[i].rows[0].cells[2].getElementsByClassName('researchPoints fontSize18 colorWhite fontBold fontColorRace bgNoRepeat 	interface_fleet_table_background')[0];
+			currPoints[i] = parseFloat(currPointsDiv.innerHTML.match(/\d+/g).join("."));
+			//production / day
+			var points = researchTables[i].rows[2].cells[0].innerHTML.match(/\d+/g).join(".");
+			productionPoints[i] = parseFloat(points);
+			
+			var extraTableDiv = researchCenterMainDiv.getElementsByClassName('extraTable')[i-1];
+			var categorieDiv = extraTableDiv.getElementsByClassName('fleetTable bgNoRepeat interface_fleet_table_background ')[0];		
+			var researchTable = categorieDiv.getElementsByClassName('colorWhite')[0];
+			for (var y = 2; y < researchTable.rows.length; y+=2)
+			{	
+				try
+				{
+					var requiredPoints = researchTable.rows[y].cells[2].innerHTML.match(/\d+/g).join("");
+					var theButton = researchTable.rows[y].cells[3];															
+					var button = theButton.getElementsByClassName('buttonError colorError interface_element_button_error')[0];	
+					var daysvalue = ((parseFloat(requiredPoints) - parseFloat(currPoints[i])) / parseFloat(productionPoints[i])).toFixed(2);
+					var days = showDays(daysvalue);
+					button.innerHTML = "<span class='interface_element_button_error'></span><div class='ColorGray'>Ready in " + days+"</div>";
 				}
-			}
-			catch (e)
-			{
-				window.status = e;
+				catch (e)
+				{
+					window.status = e;
+				}						
 			}
 		}
 	}
