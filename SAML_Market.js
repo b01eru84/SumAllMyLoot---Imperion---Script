dadd9067e9c4a26bde244bb7c7fdb060dc592b8a
Johnny Green <b01eru84@yahoo.com>
2010-01-12 23:16:44 +0200

    JohnnyGreen

--- SAML_Market.js
+++ SAML_Market.js
@@ -8,7 +8,8 @@
 
 function ShowMarketPlus()
 {
-	var isMarket = document.location.toString().match(/\bmarket\b/g);
+	var isMarket = document.location.toString().match(/\bmarket\b/g) ||
+		document.location.toString().substr(document.location.toString().length - 3, 3)=="450";
 	if (isMarket)
 	{
 		try
